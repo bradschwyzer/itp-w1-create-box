cf6889eab24128238d6a0f98df96e156481db42f
Brad Schwyzer <brad.schwyzer@gmail.com>
2017-03-25 16:09:45 +0000

    Initial Commit

--- create_box/main.py
+++ create_box/main.py
@@ -1,9 +1,31 @@
 """This is the entry point of the program."""
 
-
 def create_box(height, width, character):
-    pass
-
+    string = ''
+    if height and width > 0:
+        for h in range(height):
+            string = string + character * width + "\n"
+   
+        return string
+            
+   
 
 if __name__ == '__main__':
     create_box(3, 4, '*')
+
+
+    
+def create_box1(height, width, character):
+    string = ''
+    
+    for i in range(height):
+     if i == 0:
+        string = string + (character *(width)) + '\n'
+     elif i == (height-1):
+        string = string + (character *(width)) + '\n'
+     else:
+        string = string + (character + " " * (width-2) + character) + '\n'
+    return string
+    
+# print(create_box(10, 10, '@'))
+
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,6 @@
 import unittest
 
-from create_box import create_box
+from create_box import create_box, create_box1
 
 first_box_expected = """
 ****
@@ -18,6 +18,11 @@ xxxxxxxxxxxxxxxxxxxxxxxx
 xxxxxxxxxxxxxxxxxxxxxxxx
 """.lstrip()
 
+fourth_box_expected = '''
+****
+*  *
+****
+'''.lstrip()
 
 class TestCreateBox(unittest.TestCase):
     def test_box(self):
@@ -26,4 +31,13 @@ class TestCreateBox(unittest.TestCase):
     def test_small_box(self):
         self.assertEqual(create_box(1, 1, '@'), second_box_expected)
 
-    # Add your own test using third_box_expected
+    def test_new_box(self):
+        self.assertEqual(create_box(3,24,'x'), third_box_expected)
+
+    def test_box2(self):
+        self.assertEqual(create_box1(3,4,'*'), fourth_box_expected)
+
+"""
+https://www.tutorialspoint.com/python/string_lstrip.htm
+https://www.tutorialspoint.com/python/string_rstrip.htm
+"""
